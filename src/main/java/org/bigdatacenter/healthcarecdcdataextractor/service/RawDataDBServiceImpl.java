b3559a58e1a52a78ff6f76098ca0ca69239807dd
Anthony-Kim <anthonykim1985@gmail.com>
2018-02-20 14:03:17 +0900

    [ADD] Transactional

--- src/main/java/org/bigdatacenter/healthcarecdcdataextractor/service/RawDataDBServiceImpl.java
+++ src/main/java/org/bigdatacenter/healthcarecdcdataextractor/service/RawDataDBServiceImpl.java
@@ -5,8 +5,10 @@ import org.bigdatacenter.healthcarecdcdataextractor.domain.extraction.request.ta
 import org.bigdatacenter.healthcarecdcdataextractor.persistence.RawDataDBMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class RawDataDBServiceImpl implements RawDataDBService {
     private final RawDataDBMapper rawDataDBMapper;
 
